975e3397cc1c7e819d3f8dabd3f9402d80f464fb
ushopal <yi.yang@ushopal.com>
2023-05-10 17:32:30 +0800

    fix

--- controller/corpusVicuna.go
+++ controller/corpusVicuna.go
@@ -64,7 +64,7 @@ func AddCorpusVicuna(ctx *gin.Context) {
 	body["input"] = corpus
 
 	requestion := &res{}
-	httpStatus := curl.Post("http://region-9.seetacloud.com:41584/v1/create_embeddings", body, header, requestion)
+	httpStatus := curl.Post("http://region-9.seetacloud.com:41584/v1/embeddings", body, header, requestion)
 
 	if httpStatus != http.StatusOK {
 		logger.Error("查询失败")
@@ -129,7 +129,7 @@ func UpdateCorpusVicuna(ctx *gin.Context) {
 	body["input"] = corpus
 
 	requestion := &res{}
-	httpStatus := curl.Post("http://region-9.seetacloud.com:41584/v1/create_embeddings", body, header, requestion)
+	httpStatus := curl.Post("http://region-9.seetacloud.com:41584/v1/embeddings", body, header, requestion)
 
 	if httpStatus != http.StatusOK {
 		logger.Error("查询失败")
